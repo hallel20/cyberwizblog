90e4bbf0646153d64170ff2926b8bfe2bcaded03
Hallel Ojowuro <57284993+hallel20@users.noreply.github.com>
2024-10-30 03:57:39 +0100

    Update Admin posts map

--- app/(admin)/admin/posts/page.tsx
+++ app/(admin)/admin/posts/page.tsx
@@ -69,7 +69,7 @@ const page = async ({ searchParams }: any) => {
           </thead>
           <tbody>
             {/* Row 1 */}
-            {posts.map((post) => (
+            {posts.map((post: any) => (
               <tr key={post.id}>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                   <p className="text-gray-900 whitespace-no-wrap">
