d5c9fd614ca19229730dec7456ff1b569d09ca77
Hallel Ojowuro <57284993+hallel20@users.noreply.github.com>
2024-10-30 03:56:39 +0100

    Update Admin comments map

--- app/(admin)/admin/comments/page.tsx
+++ app/(admin)/admin/comments/page.tsx
@@ -52,7 +52,7 @@ const page = async ({ searchParams }: any) => {
           </thead>
           <tbody>
             {/* Row 1 */}
-            {comments.map((comment) => (
+            {comments.map((comment: any) => (
               <tr key={comment.id}>
                 <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                   <p className="text-gray-900 whitespace-no-wrap">
